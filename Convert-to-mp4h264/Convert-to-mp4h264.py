3d9e7cc437222d38be37223e5161ac7e1d4438dd
OnAnOpenField <38417614+OnAnOpenField@users.noreply.github.com>
2018-04-20 01:53:34 -0400

    Changed from ffmpeg to mkvmerge for file analysis

--- Convert-to-mp4h264/Convert-to-mp4h264.py
+++ Convert-to-mp4h264/Convert-to-mp4h264.py
@@ -1,8 +1,9 @@
 import subprocess
 import os
+import time
 
 VIDEO_BASEPATH = '/path/to/base-video-folder'
-AVCTrack = 'video: h264'
+AVCTRACK = 'avc/h.264'
 
 # simple function for handling fatal errors. (It outputs an error, and exits the program.)
 def fatal(errMsg):
@@ -11,15 +12,25 @@ def fatal(errMsg):
     exit()
 
 # Remux to mp4.
-def beginRemux(file):
-    filename, EXT = os.path.splitext(file)
-    TARGETFILE = file.replace(EXT, '.mp4')
+def remux(FILE):
+    FILENAME, EXT = os.path.splitext(FILE)
+    subprocess.call('ffmpeg -i "' + FILE + '" -c copy "' + FILE.replace(EXT, '.mp4') + '"', shell=True)
+    os.remove(FILE)
 
-    errcode = subprocess.call('ffmpeg -i "' + file + '" -c copy "' + TARGETFILE + '"', shell=True)
-    if errcode != 0:
-        fatal('ffmpeg has failed. Is it installed?')
+def reencode(FILE):
+    FILENAME, EXT = os.path.splitext(FILE)  # Get file extension
+    TEMPFILE = FILE.replace(EXT, '.TEMP.mp4')  # Set set temporary output filename
+    TARGETFILE = TEMPFILE.replace('.TEMP.mp4', '.mp4') # set target filename
 
-    os.remove(file)
+    FFMPEG_COMMAND = 'ffmpeg -i "' + FILE + '" -c:v libx264 -crf 18 -preset:v medium -c:a copy "' + TEMPFILE + '"'
+    HANDBRAKE_COMMAND = 'HandBrakeCLI -i "' + FILE + '" -f mp4 --aencoder copy -e qsv_h264 --x264-preset medium --x264-profile auto -q 18 --decomb bob -o "' + TEMPFILE + '"'
+
+    print('** Transcoding, Converting to H.264')
+    subprocess.call(HANDBRAKE_COMMAND, shell=True)
+
+    print('** Deleting original file')
+    os.remove(FILE)
+    os.rename(TEMPFILE, TARGETFILE)
 
 def main():
     # check if path provided exists
@@ -28,40 +39,19 @@ def main():
 
     fileList = []
     # searches for video files with specified extensions
-    for root, dirs, files in os.walk(VIDEO_BASEPATH):
-        for file in files:
-            if file.endswith('.ts') or file.endswith('.mp4') or file.endswith('.mkv') or file.endswith('.avi') or file.endswith('') or file.endswith('.m4v'):
-                fileList.append(os.path.join(root, file))
+    for ROOT, DIRS, FILENAMES in os.walk(VIDEO_BASEPATH):
+        for filename in FILENAMES:
+            if filename.endswith('.ts') or filename.endswith('.avi') or filename.endswith('.mp4') or filename.endswith('.m4v') or filename.endswith('.mkv'):
+                FILE = os.path.join(ROOT, filename)
+                if not os.path.isfile(FILE): continue
+                bOutput = subprocess.check_output('mkvmerge -i "' + FILE + '"', shell=True)
+                OUTPUT = bOutput.decode()
 
-    for file in fileList:
-        # this if statement checks if the file exists before proceeding
-        if not os.path.isfile(file):
-            continue
-
-        bOutput = subprocess.check_output('ffmpeg -i "' + file + '"', shell=True)
-        OUTPUT = bOutput.decode()
-
-        # check if file is H264 and not mp4. Remux if true
-        if AVCTrack in OUTPUT.lower() and not file.endswith('.mp4'):
-            beginRemux(file)
-            continue
-
-        # check if file is H264 encoded. Skip conversion if true
-        if AVCTrack in OUTPUT.lower():
-            continue
-
-        FILENAME, EXT = os.path.splitext(file)                      # Get file extension
-        TARGETFILE = file.replace('.' + EXT, '.mp4')                # Set the output filename
-        BAKFILE = file.replace('.' + EXT, '.BAK.' + EXT)            # Set temporary file for transcoding
-        os.rename(file, BAKFILE)                                    # Rename original file to [name].bak.[ext]
-
-        print('** Transcoding, Converting to H.264 w/Handbrake')
-        errcode = subprocess.call('HandBrakeCLI -i "' + BAKFILE + '" -f mp4 --aencoder copy -e qsv_h264 --x264-preset veryfast --x264-profile auto -q 16 --decomb bob -o "' + TARGETFILE + '"', shell=True)
-        if errcode != 0:
-            fatal("HandBrakeCLI has failed. Is it installed?")
-
-        print('** Deleting ' + BAKFILE)
-        os.remove(BAKFILE)
+                # check if file is H264 and not mp4. Remux if true, else check if file is not H264 encoded
+                if AVCTRACK in OUTPUT.lower() and not filename.endswith('.mp4'):
+                    remux(FILE)
+                elif not AVCTRACK in OUTPUT.lower():
+                    reencode(FILE)
 
 if __name__ == '__main__':
     main()
